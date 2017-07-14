ae91023dbf5532c5fd4744a49de7f6fe85ec6b9f
Chris Eady <chris.eady1@gmail.com>
2017-07-14 14:52:30 -0500

    Finish micro-reddit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :username, presence: true, length: { maximum: 20 },
+                       uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    format: VALID_EMAIL_REGEX
+  validates :password, presence: true, length: { minimum: 8 }
+
+  has_many :posts
+  has_many :comments
+end
